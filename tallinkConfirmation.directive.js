90eda8efc09020cbc1a3ed16da7a1a165ea7d6db
NeverwinterMoon <manni.calavera@gmail.com>
2015-05-20 10:27:38 +0300

    Add accordion folding for sections

--- tallinkConfirmation.directive.js
+++ tallinkConfirmation.directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tallink-confirmation')
-  .directive('confirmation', function() {
+  .directive('confirmation', function($compile) {
     return {
       restrict:      'E'
       , scope:       {
@@ -9,6 +9,46 @@ angular.module('tallink-confirmation')
         , stylesheet: '@'
       }
       , templateUrl: 'tallinkConfirmation.html'
+      , link:        linkFunction
     };
+
+    function linkFunction($scope, element) {
+      $scope.$watch('content', function(newContent) {
+        if (!newContent) return;
+
+        var forEach = Array.prototype.forEach
+          , contentEl = element.find('content')
+          ;
+
+        contentEl.html(newContent);
+        updateHeader();
+        updateBody();
+
+        $compile(contentEl.contents())($scope);
+
+        function updateHeader() {
+          var counter = 0
+            , headerEls = contentEl[0].querySelectorAll('[confirmationHeader]')
+            ;
+
+          forEach.call(headerEls, function(el) {
+            el.setAttribute('ng-click', 'showConfirmation' + counter + '=!showConfirmation' + counter);
+            counter++;
+          });
+        }
+
+        function updateBody() {
+          var counter = 0
+            , bodyEls = contentEl[0].querySelectorAll('[confirmationBody]')
+            ;
+
+          forEach.call(bodyEls, function(el) {
+            el.querySelector('div').setAttribute('ng-show', '!!!showConfirmation' + counter);
+            el.querySelector('div').setAttribute('animation', 'slide-up-down');
+            counter++;
+          });
+        }
+      });
+    }
   })
 ;
